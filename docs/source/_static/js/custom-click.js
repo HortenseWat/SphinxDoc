a57c8c80993a83a8b836166a155da1b4c0d83c3a
HortenseWat <hortense.watier@insa-strasbourg.fr>
2026-05-21 11:08:08 +0800

    Ninth commit: interaction online documantation 2

--- docs/source/_static/js/custom-click.js
+++ docs/source/_static/js/custom-click.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
-    // 1. On cherche l'élément qui contient le texte du titre en haut à gauche
     const sidebarTitle = document.querySelector(".sidebar-brand-text");
     
-    // 2. Si on a trouvé ce titre sur la page...
     if (sidebarTitle) {
+        // On change le curseur pour montrer que c'est cliquable
+        sidebarTitle.style.cursor = "pointer"; 
         
-        // 3. On remonte jusqu'à la balise de lien <a> qui l'entoure, 
-        // et on change sa destination ("href") pour pointer vers ton résumé.
-        sidebarTitle.closest("a").setAttribute("href", "C:/Users/539682/SphinxDoc/docs/build/html/index.html");
+        sidebarTitle.addEventListener("click", function() {
+            // Cette ligne renvoie TOUJOURS à la page d'accueil du site, peu importe l'URL
+            window.location.href = window.location.origin + window.location.pathname.split('/').slice(0, 2).join('/') + '/';
+        });
     }
 });
